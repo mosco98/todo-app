4e69cacc65728e6ff289a9800b92e01b83edd4de
Moscode <moscolyno98@gmail.com>
2020-10-10 13:03:28 +0100

    Added functions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,40 @@ import TodoList from './components/TodoList'
 const App = () => {
   const [todos, setTodos] = useState([])
   const [showModal, updateShowModal] = useState(false)
+
+  function AddNewTodo(newTodoObj) {
+    const newTodos = []
+    newTodos.push(...newTodos, newTodoObj)
+    setTodos(newTodos)
+  }
+
+  function MarkComplete(id) {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  function DeleteTodo(id) {
+    const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
-      {showModal && <AddTodoModal updateShowModal={updateShowModal} />}
+      {showModal && <AddTodoModal updateShowModal={updateShowModal} AddNewTodo={AddNewTodo} />}
       {todos.length ? (
         <div className="absolute top-0 right-0 m-2">
           <button className="bg-green-500 text-white text-sm rounded p-1 mx-1" onClick={() => updateShowModal(true)}>
             Add Todo
           </button>
-          <button className="bg-red-500 text-white text-sm rounded p-1 mx-1">Delete All Todos</button>
+          <button className="bg-red-500 text-white text-sm rounded p-1 mx-1" onClick={() => setTodos([])}>
+            Delete All Todos
+          </button>
         </div>
       ) : null}
       {todos.length ? (
--- src/components/AddTodoModal.js
+++ src/components/AddTodoModal.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
+import v4 from 'uuid'
 
-const AddTodoModal = ({ updateShowModal }) => {
+const AddTodoModal = ({ updateShowModal, AddNewTodo }) => {
   const [title, setTitle] = useState('')
+
+  function FormSubmitHandler(e) {
+    e.preventDefault()
+
+    if (title === '') return
+
+    const newTodo = {
+      id: v4(),
+      title,
+      completed: false
+    }
+
+    AddNewTodo(newTodo)
+    updateShowModal(false)
+  }
   return (
     <>
       <div className="overlay" onClick={() => updateShowModal(false)} />
-      <form className="add-todo-form shadow-sm p-8">
-        <input type="text" className="p-2" placeholder="Enter new todo" autoFocus={true} />
+      <form className="add-todo-form shadow-sm p-8" onSubmit={FormSubmitHandler}>
+        <input
+          type="text"
+          className="p-2"
+          placeholder="Enter new todo"
+          autoFocus={true}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <button className="bg-green-500 text-white text-sm rounded p-2 mx-1">Add todo</button>
       </form>
     </>
